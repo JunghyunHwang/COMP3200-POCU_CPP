4049be9efdd7260ac6ee44f2223c8c9e087199ad
junghyunhwang <dmagk560@gmail.com>
2022-09-05 17:47:15 +0900

    Remove assignment1

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,15 +1,4 @@
-#include <iostream>
-
-#include "MyString.h"
-
-using namespace std;
-using namespace assignment1;
-
 int main()
 {
-    MyString str("POCU");
 
-    cout << str.GetCString() << endl;
-
-    return 0;
 }
