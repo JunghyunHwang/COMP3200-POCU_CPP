6d53d56936662bcf915f9965b2d4a493f1f85e84
junghyunhwang <dmagk560@gmail.com>
2022-09-05 17:23:19 +0900

    Add assignment1 and white space test

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+
+#include "MyString.h"
+
+using namespace std;
+using namespace assignment1;
+
 int main()
 {
+    MyString str("POCU");
 
+    cout << str.GetCString() << endl;
+
+    return 0;
 }
