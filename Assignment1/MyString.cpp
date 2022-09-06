557c33800bc377f5bc83bda133d7cc745990a0b7
Ja_Hwang <dmagk560@gmail.com>
2022-09-06 21:58:55 +0900

    Add assignment1

---
+++ Assignment1/MyString.cpp
@@ -0,0 +1,121 @@
+#include "MyString.h"
+
+namespace assignment1
+{
+	MyString::MyString(const char* s)
+		: mCount(0)
+		, mCapacity(DEFAULT_CAPACITY)
+	{
+		const char* pS = s;
+
+		while (*pS++ != '\0')
+		{
+			++mCount;
+		}
+
+		if (mCount > mCapacity)
+		{
+			mCapacity = mCount * 2;
+		}
+
+		mStr = new char[mCapacity];
+
+		pS = s;
+		char* pStr = mStr;
+
+		while (*pS != '\0')
+		{
+			*pStr++ = *pS++;
+		}
+
+		*pStr = '\0';
+	}
+
+	MyString::MyString(const MyString& other)
+		: mCount(other.mCount)
+		, mCapacity(other.mCapacity)
+	{
+		mStr = new char[mCapacity];
+
+		const char* pOtherString = other.mStr;
+		char* pStr = mStr;
+
+		while (*pOtherString != '\0')
+		{
+			*pStr++ = *pOtherString++;
+		}
+
+		*pStr = '\0';
+	}
+
+	MyString::~MyString()
+	{
+		delete[] mStr;
+	}
+
+	unsigned int MyString::GetLength() const
+	{
+		return mCount;
+	}
+
+	const char* MyString::GetCString() const
+	{
+		return mStr;
+	}
+
+	void MyString::Append(const char* s)
+	{
+		const char* pS = s;
+		int sLength = 0;
+
+		while (*pS++ != '\0')
+		{
+			++sLength;
+		}
+
+		if (mCapacity < mCount + sLength + 1)
+		{
+			mCapacity = (mCount + sLength) * 2;
+
+			char* tmp = new char[mCapacity];
+			char* pTmp = tmp;
+			const char* pStr = mStr;
+
+			while (*pStr != '\0')
+			{
+				*pTmp++ = *pStr++;
+			}
+
+			delete[] mStr;
+			mStr = tmp;
+		}
+
+		pS = s;
+
+		for (size_t i = 0; i < sLength; ++i)
+		{
+			mStr[mCount++] = *pS++;
+		}
+
+		mStr[mCount] = '\0';
+	}
+
+	bool MyString::operator==(const MyString& rhs) const
+	{
+		const char* pStr = mStr;
+		const char* pRhs = rhs.mStr;
+
+		while (*pStr != '\0' && *pStr == *pRhs)
+		{
+			++pStr;
+			++pRhs;
+		}
+
+		if (*pStr != '\0')
+		{
+			return false;
+		}
+
+		return true;
+	}
+}
--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,4 +1,82 @@
+#include <iostream>
+#include <cassert>
+#include <cstring>
+
+#include "MyString.h"
+
+using namespace std;
+using namespace assignment1;
+
+void testConstructor();
+void testCopyConstructor();
+void testAppend();
+
 int main()
 {
+	testConstructor();
+	testAppend();
+	testCopyConstructor();
 
+	cout << "No prob" << endl;
+
+	return 0;
+}
+
+void testConstructor()
+{
+	MyString str1("POCU");
+	string cmp(str1.GetCString());
+
+	assert(str1.GetLength() == 4);
+	assert(cmp == "POCU");
+
+	MyString str2("Give me the beat I ride it like a jectski");
+	cmp = str2.GetCString();
+
+	assert(str2.GetLength() == 41);
+	assert(cmp == "Give me the beat I ride it like a jectski");
+}
+
+void testAppend()
+{
+	MyString str1("POCU");
+	string cmp(str1.GetCString());
+
+	assert(str1.GetLength() == 4);
+	assert(cmp == "POCU");
+
+	str1.Append("DA");
+	cmp = str1.GetCString();
+
+	assert(str1.GetLength() == 6);
+	assert(cmp == "POCUDA");
+
+	str1.Append(" Season change and our love went cold");
+	cmp = str1.GetCString();
+
+	assert(str1.GetLength() == 43);
+	assert(cmp == "POCUDA Season change and our love went cold");
+}
+
+void testCopyConstructor()
+{
+	MyString str1("POCU");
+	MyString str2(str1);
+	string cmp1(str1.GetCString());
+	string cmp2(str2.GetCString());
+
+	assert(str1.GetLength() == 4);
+	assert(cmp1 == "POCU");
+
+	assert(str2.GetLength() == 4);
+	assert(cmp2 == "POCU");
+
+	str1.Append("DAT");
+	cmp1 = str1.GetCString();
+
+	assert(str1.GetLength() == 7);
+	assert(cmp1 == "POCUDAT");
+
+	assert(str2.GetLength() == 4);
+	assert(cmp2 == "POCU");
 }
