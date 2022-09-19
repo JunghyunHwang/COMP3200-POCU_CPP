9db63e7ed452c6e9521f400cdd8731517388320f
junghyunhwang <dmagk560@gmail.com>
2022-09-19 19:46:07 +0900

    Fix space to tab

--- Lab3/TimeSheet.cpp
+++ Lab3/TimeSheet.cpp
@@ -3,109 +3,109 @@
 
 namespace lab3
 {
-    TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
-        : mCount(0)
-        , mMaxEntries(maxEntries)
-        , mTotalTime(0)
-    {
-        mTimes = new unsigned int[maxEntries];
-        memset(mTimes, 0, sizeof(int) * maxEntries);
-        mName = name;
-    }
+	TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
+		: mCount(0)
+		, mMaxEntries(maxEntries)
+		, mTotalTime(0)
+	{
+		mTimes = new unsigned int[maxEntries];
+		memset(mTimes, 0, sizeof(int) * maxEntries);
+		mName = name;
+	}
 
-    TimeSheet::TimeSheet(const TimeSheet& other)
-        : mCount(other.mCount)
-        , mMaxEntries(other.mMaxEntries)
-        , mTotalTime(other.mTotalTime)
-        , mName(other.mName)
-    {
-        mTimes = new unsigned int[mMaxEntries];
-        memcpy(mTimes, other.mTimes, sizeof(unsigned int) * mMaxEntries);
-    }
+	TimeSheet::TimeSheet(const TimeSheet& other)
+		: mCount(other.mCount)
+		, mMaxEntries(other.mMaxEntries)
+		, mTotalTime(other.mTotalTime)
+		, mName(other.mName)
+	{
+		mTimes = new unsigned int[mMaxEntries];
+		memcpy(mTimes, other.mTimes, sizeof(unsigned int) * mMaxEntries);
+	}
 
-    TimeSheet::~TimeSheet()
-    {
-        delete[] mTimes;
-    }
+	TimeSheet::~TimeSheet()
+	{
+		delete[] mTimes;
+	}
 
-    TimeSheet& TimeSheet::operator=(const TimeSheet& rhs)
-    {
-        if (mTimes == rhs.mTimes)
-        {
-            return *this;
-        }
-        
-        delete[] mTimes;
+	TimeSheet& TimeSheet::operator=(const TimeSheet& rhs)
+	{
+		if (mTimes == rhs.mTimes)
+		{
+			return *this;
+		}
+		
+		delete[] mTimes;
 
-        mCount = rhs.mCount;
-        mMaxEntries = rhs.mMaxEntries;
-        mTotalTime = rhs.mTotalTime;
-        mName = rhs.mName;
+		mCount = rhs.mCount;
+		mMaxEntries = rhs.mMaxEntries;
+		mTotalTime = rhs.mTotalTime;
+		mName = rhs.mName;
 
-        mTimes = new unsigned int[rhs.mMaxEntries];
-        memcpy(mTimes, rhs.mTimes, sizeof(unsigned int) * mMaxEntries);
+		mTimes = new unsigned int[rhs.mMaxEntries];
+		memcpy(mTimes, rhs.mTimes, sizeof(unsigned int) * mMaxEntries);
 
-        return *this;
-    }
+		return *this;
+	}
 
-    void TimeSheet::AddTime(int timeInHours)
-    {
-        if (timeInHours < 1 || timeInHours > 10 || mCount >= mMaxEntries)
-        {
-            return;
-        }
+	void TimeSheet::AddTime(int timeInHours)
+	{
+		if (timeInHours < 1 || timeInHours > 10 || mCount >= mMaxEntries)
+		{
+			return;
+		}
 
-        mTimes[mCount++] = timeInHours;
-        mTotalTime += timeInHours;
-    }
+		mTimes[mCount++] = timeInHours;
+		mTotalTime += timeInHours;
+	}
 
-    int TimeSheet::GetTimeEntry(unsigned int index) const
-    {
-        if (index >= mCount)
-        {
-            return -1;
-        }
+	int TimeSheet::GetTimeEntry(unsigned int index) const
+	{
+		if (index >= mCount)
+		{
+			return -1;
+		}
 
-        return mTimes[index];
-    }
+		return mTimes[index];
+	}
 
-    int TimeSheet::GetTotalTime() const
-    {
-        return mTotalTime;
-    }
+	int TimeSheet::GetTotalTime() const
+	{
+		return mTotalTime;
+	}
 
-    float TimeSheet::GetAverageTime() const
-    {
-        if (mTotalTime == 0)
-        {
-            return 0.0;
-        }
-        
-        return mTotalTime / (float)mCount;
-    }
+	float TimeSheet::GetAverageTime() const
+	{
+		if (mTotalTime == 0)
+		{
+			return 0.0;
+		}
+		
+		return mTotalTime / (float)mCount;
+	}
 
-    float TimeSheet::GetStandardDeviation() const
-    {
-        if (mCount == 0) 
-        {
-            return 0.0;
-        }
+	float TimeSheet::GetStandardDeviation() const
+	{
+		if (mCount == 0) 
+		{
+			return 0.0;
+		}
 
-        unsigned int squaredSum = 0;
+		unsigned int squaredSum = 0;
 
-        for (size_t i = 0; i < mCount; ++i) 
-        {
-            squaredSum += mTimes[i] * mTimes[i];
-        }
+		for (size_t i = 0; i < mCount; ++i) 
+		{
+			squaredSum += mTimes[i] * mTimes[i];
+		}
 
-        float average = GetAverageTime();
-        float deviation = squaredSum / (float)mCount - average * average;
+		float average = GetAverageTime();
+		float deviation = squaredSum / (float)mCount - average * average;
 
-        return sqrt(deviation);
-    }
+		return sqrt(deviation);
+	}
 
-    const std::string& TimeSheet::GetName() const
-    {
-        return mName;
-    }
+	const std::string& TimeSheet::GetName() const
+	{
+		return mName;
+	}
 }
