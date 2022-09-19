b6698dbb5265b1ac270c4ad07be8954ecdd59f38
junghyunhwang <dmagk560@gmail.com>
2022-09-19 19:33:17 +0900

    Add lab 3

---
+++ Lab3/TimeSheet.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+
+namespace lab3
+{
+    class TimeSheet
+    {
+    public:
+        TimeSheet(const char* name, unsigned int maxEntries);
+        TimeSheet(const TimeSheet& other);
+        virtual ~TimeSheet();
+        void AddTime(int timeInHours);
+        int GetTimeEntry(unsigned int index) const;
+        int GetTotalTime() const;
+        float GetAverageTime() const;
+        float GetStandardDeviation() const;
+        const std::string& GetName() const;
+        TimeSheet& operator=(const TimeSheet& rhs);
+
+    private:
+       unsigned int mCount;
+       unsigned int mMaxEntries;
+       unsigned int mTotalTime;
+       unsigned int* mTimes;
+       std::string mName;
+    };
+}
