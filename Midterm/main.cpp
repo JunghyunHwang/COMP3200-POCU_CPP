40874dd267c8c74c8f6d5f1923e9b4b1cb7d679a
Ja_Hwang <dmagk560@gmail.com>
2022-10-26 00:36:51 +0900

    Add midterm practice 2

--- Midterm/main.cpp
+++ Midterm/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
 
-#include "MyString.h"
-using namespace practice;
+void output1()
+{
+    std::cout << std::showpos << std::uppercase << std::showbase << std::setw(10) << std::setprecision(4) << std::hex << std::setfill('?');
+    std::cout << 3.141592 << std::endl << std::setw(15) << 16;
+    std::cout << std::setw(5) << std::setfill('-') << ' ';
+}
+
 
 int main()
 {
-	MyString s("Hello, World");
-
-	s += " Hello, World";
-
-	std::cout << s << std::endl;
-
-	return 0;
+    output1();
 }
