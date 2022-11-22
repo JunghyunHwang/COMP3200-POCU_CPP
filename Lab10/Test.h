4316d7514718e1a7116b3ddc79d941f4ec03d104
junghyunhwang <dmagk560@gmail.com>
2022-11-22 17:38:01 +0900

    Add wiki test

--- Lab10/Test.h
+++ Lab10/Test.h
@@ -2,14 +2,21 @@
 
 #include <cassert>
 #include <memory>
+#include <crtdbg.h>
 #include <iostream>
 
 #include "Node.h"
 #include "DoubleLinkedList.h"
 
+#ifdef _DEBUG
+#define new new(_CLIENT_BLOCK, __FILE__, __LINE__)
+#define malloc(s) _malloc_dbg(s, _NORMAL_BLOCK, __FILE__, __LINE__)
+#endif
+
 namespace lab10
 {
 	void TestOfficial();
+	void TestWiki();
 	void TestInsert();
 	void TestDelete();
 	void TestSearch();
