b31c7947860fec4fa19b00b6fb1718cf837f819e
Ja_Hwang <dmagk560@gmail.com>
2022-11-22 13:49:12 +0900

    Add assignment 4

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+#include <memory>
+
+#include "TreeNode.h"
+
+using namespace assignment4;
+
 int main()
 {
-
+	TreeNode<int> n(std::make_unique<int>(2));
+	std::cout << "No prob" << std::endl;
+	return 0;
 }
